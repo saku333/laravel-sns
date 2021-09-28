aae19be6245e2769314995ba1ebd7399fe3145fd
saku333 <sakura.ym3@gmail.com>
2021-09-29 00:13:54 +0900

    Add ArticleController, app.blade.php, index.blade.js

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ArticleController::class,'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
